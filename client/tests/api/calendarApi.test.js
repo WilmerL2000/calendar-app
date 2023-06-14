186f67c7a06180a1308eba9519974ff5715af344
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-06-14 17:44:21 -0600

    Setting the files and configuration to use jest for teting

---
+++ client/tests/api/calendarApi.test.js
@@ -0,0 +1,7 @@
+// import calendarApi from '../../src/api/calendarApi';
+
+describe('Pruebas en calendarApi', () => {
+  test('Debe de tener la configuracion por defecto ', async () => {
+    // calendarApi;
+  });
+});
