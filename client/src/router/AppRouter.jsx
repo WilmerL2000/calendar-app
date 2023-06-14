08430c5808c24a609cdd8ffd14b9269d98dbc3da
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-06-14 10:34:39 -0600

    Modified routes

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -24,7 +24,7 @@ export const AppRouter = () => {
         </>
       ) : (
         <>
-          <Route path="/auth" element={<Navigate to="/auth/login" />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
           <Route path="/auth/*" element={<LoginPage />} />
         </>
       )}
